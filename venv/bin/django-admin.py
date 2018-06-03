8f362c31929120b0bdfb5d10a368dc2e32faa283
Eslam Elsawy <eslam@uw.edu>
2018-06-02 18:26:48 -0700

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/eslamelsawy/Desktop/CLMS/575_Egypt/project/pentimento/teigeneratortool/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
